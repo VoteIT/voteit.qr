0b7f3836417931e05bcff9605dd759ddcf78b755
Robin Harms Oredsson <robin@betahaus.net>
2018-02-08 14:00:06 +0100

    Cleanup

--- voteit/qr/schemas.py
+++ voteit/qr/schemas.py
@@ -3,14 +3,6 @@ from __future__ import unicode_literals
 
 import colander
 import deform
-from arche.schemas import userid_hinder_widget
-from arche.validators import existing_userids, ExistingUserIDs
-from pyramid.decorator import reify
-from pyramid.httpexceptions import HTTPNotFound
-
-from voteit.vote_groups import _
-from voteit.vote_groups.interfaces import IVoteGroups
-from voteit.vote_groups.mixins import VoteGroupEditMixin
 
 
 def includeme(config):
